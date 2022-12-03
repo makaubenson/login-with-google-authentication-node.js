fcdbe20861c193c25e8fce9e4f9693357f233c57
benson <bensonmakau2000@gmail.com>
2022-12-04 00:14:02 +0300

    Design views

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 //@desc Login/Landing Page
 //@route GET /
 router.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    layout: "login",
+  });
 });
 
 //@desc Dashboard
