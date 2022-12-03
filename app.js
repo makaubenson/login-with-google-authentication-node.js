61de89100810af820d3740e2561eb39fd20fcb60
benson <bensonmakau2000@gmail.com>
2022-12-03 23:32:54 +0300

    db connection and logging

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //Load Config File
 dotenv.config({ path: "./config/config.env" });
 
+//call DB connection Function
+connectDB();
+
 //initialize app
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(
   PORT,
