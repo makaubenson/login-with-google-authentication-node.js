be610f8d9154f02dcf2e58f5302ba05f0b26ee82
benson <bensonmakau2000@gmail.com>
2022-12-12 14:21:32 +0300

    edit and update story

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -25,13 +26,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//METHOD OVERRIDE
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 //logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 //Handlebars Helpers
-const { formatDate } = require("./helpers/hbs");
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
 
 //handlebars
 app.engine(
@@ -39,6 +58,10 @@ app.engine(
   exphbs.engine({
     helpers: {
       formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
     },
     defaultLayout: "main",
     extname: ".hbs",
@@ -62,6 +85,12 @@ app.use(
 //passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
--- routes/stories.js
+++ routes/stories.js
@@ -23,4 +23,64 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+//@desc Show all stories
+//@route GET /stories
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    //fetch stories
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("/error/500");
+  }
+});
+
+//@desc Show edit page
+//@route GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  const story = await Story.findOne({
+    _id: req.params.id,
+  }).lean();
+
+  console.log(story);
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    res.render("stories/edit", {
+      story,
+    });
+  }
+});
+
+//@desc Update story
+//@route PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.redirect("/dashboard");
+  }
+});
+
 module.exports = router;
