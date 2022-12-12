cc19864e26df18c9e13b33abdaee8539cf4d7f50
benson <bensonmakau2000@gmail.com>
2022-12-12 12:15:34 +0300

    creatimg stories

--- app.js
+++ app.js
@@ -21,13 +21,29 @@ connectDB();
 //initialize app
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 //logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+//Handlebars Helpers
+const { formatDate } = require("./helpers/hbs");
+
 //handlebars
-app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    helpers: {
+      formatDate,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 //Sessions
@@ -53,6 +69,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
